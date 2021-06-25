4f98e6b037d068ec363686ede956a7dd8bf0985d
dh_2dsy5c <dh_2dsy5c@ps590344.dreamhostps.com>
2021-06-25 04:45:34 -0700

    again changes

--- resources/views/layouts/user1_layout.blade.php
+++ resources/views/layouts/user1_layout.blade.php
@@ -17,9 +17,9 @@
     <link href="{{asset('new-panel/user-panel/assets/file-input/fileinput.css')}}" media="all" rel="stylesheet" type="text/css"/>
     <link href="{{asset('new-panel/user-panel/assets/file-input/theme.css')}}" media="all" rel="stylesheet" type="text/css"/>
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key={{env("GOOGLE_API")}}&libraries=places" ></script>
-    <script type="text/javascript" src="{{asset('new-panel\user-panel/assets\scripts\main.js')}}"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
 
+    <script type="text/javascript" src="{{asset('new-panel\user-panel/assets\scripts\main.js')}}"></script>
 
 
     @yield('css')
