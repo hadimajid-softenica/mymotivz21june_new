526b468dcde51a6bae1b237ce8458d80d976b4d4
hadimajid-softenica <hadimajidsoftenica@gmail.com>
2021-06-25 14:56:28 +0500

    api changes v2

--- resources/views/layouts/company.blade.php
+++ resources/views/layouts/company.blade.php
@@ -30,7 +30,8 @@
 
     <script  type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key={{env('GOOGLE_API')}}&libraries=places" ></script>
     <script src="{{asset('google-map.js')}}"></script>
-    <script type="text/javascript" src="{{asset('new-panel/company-panel/assets/scripts/main.js')}}"></script>
+{{--    <script type="text/javascript" src="{{asset('new-panel/company-panel/assets/scripts/main.js')}}"></script>--}}
+    <script type="text/javascript" src="{{asset('assets/scripts/main.js')}}"></script>
 
 </head>
 
